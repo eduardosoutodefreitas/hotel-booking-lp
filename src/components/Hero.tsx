2e726a177c5d62234170fa9b35a92abd50b8df53
eduardosoutodefreitas <dev.edufreitas@gmail.com>
2024-02-23 14:47:20 -0300

    feat: add initial setup and assets

---
+++ src/components/Hero.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section className='pt-23 pb-12 xl:py-0 xl:h-[1087px] bg-hero2 xl:bg-hero bg-cover bg-center bg-no-repeat flex justify-center items-center'>
+      Hero
+    </section>
+  );
+};
+
+export default Hero;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,64 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
+    },
+    colors: {
+      white: "#fff",
+      black: "#273029",
+      soft_green: {
+        DEFAULT: "#85c7dc",
+        secondary: "#f3f9fb",
+      },
+      grey: {
+        DEFAULT: "#888",
+        secondary: "#f8f8f8",
+      },
+      orange: "#f2994a",
+      outline: "#f1f1f1",
+      pink: "#f3d1d7",
+      body: "#e5e5e5",
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
+
+    backgroundImage: {
+      hero: "url:('assets/hero/bg.png')",
+      hero2: "url:('assets/hero/bg-2.png')",
+    },
   },
-  plugins: [],
-};
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
+
 export default config;
